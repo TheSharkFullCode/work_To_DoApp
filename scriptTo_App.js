84d6d0362d76a49f633a218ff097b453c1ae3542
TheSharkFullCode <agedjoscar@hotmail.com>
2023-07-29 02:03:45 +0200

    update

--- scriptTo_App.js
+++ scriptTo_App.js
@@ -41,37 +41,69 @@ butonSubmit1.style.width = '50px'
 const butonSubmit2 = document.createElement('button')
 butonSubmit2.setAttribute('type','button')
 butonSubmit2.id = 'botonDelete'  // crea un elemento de tipo botón
-butonSubmit2.textContent = 'Delete'
+butonSubmit2.textContent = 'Clear'
 miDiv.append(butonSubmit2)
 butonSubmit2.style.height = '20px' // crea un elemento de tipo botón
 
 // formulario.addEventListener('submit', function (event){})
 
+function agregarElemento() {
+    const inputValue = valueInput.value;
+    const nuevoElemento = document.createElement('li');
+    nuevoElemento.textContent = inputValue;
+    listaDesordenada.appendChild(nuevoElemento);
+    textSpanvalue.textContent = `${inputValue}`
+  }
+
+const textSpanvalue = document.createElement('span')
+const listaDesordenada = document.createElement('ul')
+body.append(textSpanvalue)
+console.log(textSpanvalue);
 
 butonSubmit1.addEventListener('click',()=>{
-    const inputValue = document.getElementById('valueInput').value
-    const textSpanvalue = document.createElement('span')
-    textSpanvalue.textContent = inputValue
-    miDiv.append(textSpanvalue)
-    console.log(textSpanvalue);
-    listaDesordenada.innerHTML=`<li>${inputValue}`
-    elementLi2.textContent = `<li>${inputValue}`
-    
+    agregarElemento();
 
 })
 
+
+const buttonBorrar = document.getElementById('botonDelete')
+
+function borrarValor(){
+    valueInput.value = "";
+}
+
+buttonBorrar.addEventListener('click', ()=>{
+    borrarValor();
+})
+
+
+
 const addTextSpan = document.createElement('span')
 // addTextSpan.textContent= 'hello world'
-const listaDesordenada = document.createElement('ul')
-const elementLi = document.createElement('li')
 const elementLi2 = document.createElement('li')
-elementLi.classList.add("itemLi")   // agrega una clase a la etiqueta
+// elementLi.classList.add("itemLi")
+   // agrega una clase a la etiqueta
 miDiv.appendChild(listaDesordenada)
-elementLi.textContent='Elemento Creado por JS'
+// elementLi.textContent='Elemento Creado por JS'
 console.log(addTextSpan);
 miDiv.appendChild(addTextSpan);
 
+// ---------------------------------------------
+const buttonEliminar = document.createElement('button')
+buttonEliminar.setAttribute('id','btnDelete');
+buttonEliminar.textContent = 'Delete'
+miDiv.append(buttonEliminar)
+// ---------------------------------
+buttonEliminar.style.height = '50px'
 
+buttonEliminar.addEventListener('click',eliminarElemento)
+function eliminarElemento(){
+    //verificar si la lista tiene elementos.
+if(listaDesordenada.children.length > 0){
+ listaDesordenada.remove(listaDesordenada.firstChild)
+
+}
+}
 
 
 
